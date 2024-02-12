1a273774404475e4e9823b8a12aaceed47edb8fa
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-02-12 18:57:01 +0300

    Initial commit

---
+++ src/main/java/ru/andrey/springProject2Boot/SpringProject2BootApplication.java
@@ -0,0 +1,13 @@
+package ru.andrey.springProject2Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject2BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject2BootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrey/springProject2Boot/SpringProject2BootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrey.springProject2Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
